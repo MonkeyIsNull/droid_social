84d521141ad9a06caaa57501916a73869a31d10b
Adam Guyot <adam@cobenian.com>
2015-11-02 21:01:46 -0500

    unmocked

--- test/droid_social_test.exs
+++ test/droid_social_test.exs
@@ -2,21 +2,6 @@ defmodule DroidSocialTest do
   use ExUnit.Case
   doctest DroidSocial
 
-  import :meck
-
-  setup_all do
-    new(DroidProtocol)
-    on_exit fn -> unload end
-    :ok
-  end
-
-  test "mocked protocol" do
-    mock_resp = "boop: Hiyas"
-    expect(DroidProtocol, :beep, fn("Hiyas") -> mock_resp end)
-    assert validate(DroidProtocol)
-    assert DroidSocial.post("Hiyas") == "boop: Hiyas"
-  end
-
   test "raw protocol" do
     assert DroidSocial.post("Hiyas") == "boop: Hiyas"
   end
